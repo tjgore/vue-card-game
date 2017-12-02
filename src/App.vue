94926efb4e99ef71bdac9c6cafe177db5f20e028
Tyndale Gore <gorejrtyndale1@gmail.com>
2017-12-02 14:07:20 +0800

    Initial commit: initial game

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  <div class="col-xs-12">
+  <h1 class="text-center"><strong>Card Matching</strong></h1>
+  </div>
+  <cards></cards>
+  </div>
+</template>
+
+<script>
+import Cards from './components/Card.vue'
+export default{
+   
+   components:{
+    Cards 
+   }
+    }
+  
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,213 @@
+<template>
+<div class="col-xs-12 text-center cards-container">
+<h2>Level {{ difficulty }}</h2>
+<p v-for="(cards,index) in allCards" class="card text-center" :class="{cardplay:cards.action}" @click="flip(cards,index)"><i class="fa fa-3x icon"  :class="cards.icon"></i></p>
+<div class="col-xs-12 btn-container">
+<button class="btn btn-primary btn-lg" @click="reshuffle('reset')">Restart</button>
+</div>
+<div class="col-xs-12 ">
+<p>{{matches}}</p>
+</div>
+<div v-if="win" class="faded"></div>
+<div  v-if="win" class="win-box">
+<div class="col-xs-12 congrats" v-if="win">
+<h1><strong>You Win!!</strong></h1>
+<button class="btn btn-success btn-lg" @click="reshuffle('levelup')">Next</button>
+</div>
+</div>
+
+</div>
+
+
+
+
+</template>
+
+<script>
+
+export default{
+
+	data:
+		function(){
+			return {
+				
+				cardsInfo:[
+				{id:1, icon:"fa-eye", action:false},
+				{id:2, icon:"fa-balance-scale", action:false},
+				{id:3, icon:"fa-bath", action:false},
+				{id:4, icon:"fa-bug", action:false},
+				{id:5, icon:"fa-camera", action:false},
+				{id:6, icon:"fa-coffee", action:false},
+				{id:7, icon:"fa-cubes", action:false},
+				{id:8, icon:"fa-eraser", action:false},
+				{id:9, icon:"fa-flash", action:false},
+				{id:10, icon:"fa-heartbeat", action:false},
+				{id:11, icon:"fa-gavel", action:false},
+				{id:12, icon:"fa-handshake-o", action:false},
+				{id:13, icon:"fa-music", action:false},
+				{id:14, icon:"fa-shopping-basket", action:false},
+				{id:15, icon:"fa-send", action:false},
+				{id:16, icon:"fa-star", action:false},
+				{id:17, icon:"fa-tv", action:false},
+				{id:18, icon:"fa-tree", action:false},
+				{id:19, icon:"fa-rocket", action:false},
+				{id:20, icon:"fa-github-alt", action:false}
+				
+
+				],
+				difficulty:1,
+				cardsCount:4,
+				timer:'00:00',
+				levelMatch:2,
+				allCards:[],
+				match: [],
+				attempts: 0,
+				matches:0,
+				win:false,
+				points:0				
+			}
+		},
+
+		methods:{
+			flip(obj,index){
+				if(this.match.length==this.levelMatch){
+					return;
+				}
+				
+				obj={id:obj.id, icon:obj.icon, action:true};
+				
+				this.allCards.splice(index,1,obj);
+				//console.log(this.allCards);
+				
+				this.match.push(obj);
+
+				if(this.match.length==this.levelMatch){
+					setTimeout( this.check,1200);
+				}
+
+
+				
+			},
+			check(){
+				
+
+				if(this.match[0].id==this.match[1].id ){
+					this.matches++;
+					this.points+=2;
+					this.match.splice(0,2);
+					if(this.matches==this.cardsCount){
+					this.win=true;
+					return;
+				}
+
+				}else {
+					this.attempts++;
+					for(var key=0;key<this.allCards.length;key++){
+						if(this.allCards[key].id==this.match[0].id || this.allCards[key].id==this.match[1].id){
+							this.allCards.splice(key,1,{id:this.allCards[key].id, icon:this.allCards[key].icon, action:false });
+						}
+					}
+					
+					this.match.splice(0,2);
+					
+				}
+			
+			},
+			increaseDifficulty(){
+				this.cardsCount+=2;
+				this.addMoreCards();
+				if(this.difficulty==10){
+					this.cardsCount=10;
+					this.addMoreCards();
+				}
+			},
+			reshuffle(state){
+				//this.allCards.push({id:11, icon:"fa-automobile"});
+				
+
+				if(state=='reset'){
+					var opt = this.shuffle();
+					for(var i=0;i<this.allCards.length;i++){
+						this.$set(this.allCards, i, {id:opt[i].id, icon:opt[i].icon, action:false});
+					}
+				}else if(state=='levelup'){
+					this.difficulty++;
+					this.increaseDifficulty();
+					var opt = this.shuffle();
+					for(var i=0;i<this.allCards.length;i++){
+						this.$set(this.allCards, i, {id:opt[i].id, icon:opt[i].icon, action:false});
+					}
+				}
+				this.matches=0;
+				this.attempts=0;
+				this.win=false;
+			},
+			shuffle(){
+				var length = this.allCards.length-1;
+				var temp, random;
+
+				while(length>=0){
+					random = Math.floor(Math.random() * length+1);
+
+					temp = this.allCards[length];
+					this.allCards[length] = this.allCards[random];
+					this.allCards[random] = temp;
+					this.allCards[length].action = false;
+					length--;
+
+				}
+				//console.log(this.allCards);
+				return this.allCards;
+			},
+			addMoreCards(){
+				this.allCards=[];
+				for(var matchcount=0;matchcount<this.levelMatch;matchcount++){
+					for (var key=0; key<this.cardsCount; key++){
+					this.allCards.push(this.cardsInfo[key]);
+					}
+				}
+				this.shuffle();
+			}
+			
+		},
+		created(){
+			for(var matchcount=0;matchcount<this.levelMatch;matchcount++){
+				for (var key=0; key<this.cardsCount; key++){
+				this.allCards.push(this.cardsInfo[key]);
+				}
+			}
+			
+			this.shuffle();
+			
+		}
+	
+}
+</script>
+
+<style scoped>
+.card{ display:inline-block; width:100px; height:100px; border:1px solid gray;padding:10px; margin:5px;background-color: green; }
+.cardplay{ animation-name:example; animation-duration:1s;animation-fill-mode: forwards;}
+.cardplay2{  animation-name:example; animation-duration:1s;animation-fill-mode: forwards; }
+.cardplay .icon{  animation-name:icon; animation-duration:1s;animation-fill-mode: forwards; }
+.icon{ opacity:0; }
+.fa{padding:20px;color:white;}
+.cards-container{ padding-top:30px;}
+.btn-container{ padding-top:50px;}
+
+@keyframes example {
+0% {transform:rotateY(0deg);}
+50% {transform:rotateY(90deg);}
+100% {transform:rotateY(180deg);}
+}
+
+@keyframes icon {
+0% {opacity:0;}
+45% {opacity:0;}
+50% {opacity:0;}
+55% {opacity:1;}
+100% {opacity:1;}
+}
+
+.faded{position:fixed;top:0;left:0;width:100%;height:100%;z-index:49;background-color:#00000087;}
+.win-box{position:fixed;top:10%;left:10%;width:80%;height:40%;z-index:50;background-color:white;border:1px solid gray; padding-top:45px;}
+</style>
